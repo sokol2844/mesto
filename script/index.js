fc3074ede0d6e287d17415ae84c384ebca2f19f0
Aleksander Sokolov <cokol.cokolov@yandex.ru>
2020-09-11 12:37:41 +0300

    fixed for first comments

--- script/index.js
+++ script/index.js
@@ -2,37 +2,27 @@ let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__container');
 let buttonReset = document.querySelector('.popup__button-reset');
 let buttonEdit = document.querySelector('.profile__edit-button');
+let labelName = document.querySelector('.profile__title');
+let labelDescription = document.querySelector('.profile__subtitle');
+let inputName = document.querySelector('.popup__input_name');
+let inputDescription = document.querySelector('.popup__input_about');
 
-
-
-
-function formToggle() {
-	popup.classList.toggle('popup_opened');
-}
 
 function formOpen() {
-	let labelName = document.querySelector('.profile__title');
-	let labelDescription = document.querySelector('.profile__subtitle');
-	let inputName = document.querySelector('.popup__input_name');
-	let inputDescription = document.querySelector('.popup__input_about');
 	inputName.value = labelName.textContent;
 	inputDescription.value = labelDescription.textContent;
-	formToggle();
-	//popup.classList.add('popup_opened');
+	popup.classList.add('popup_opened');
 }
 
 function formSubmit(evt) {
 	evt.preventDefault();
-	let labelName = document.querySelector('.profile__title');
-	let labelDescription = document.querySelector('.profile__subtitle');
-	let inputName = document.querySelector('.popup__input_name');
-	let inputDescription = document.querySelector('.popup__input_about');
 	labelName.textContent = inputName.value;
 	labelDescription.textContent = inputDescription.value;
-	formToggle();
-	//popup.classList.remove('popup_opened');
+	popup.classList.remove('popup_opened');
 }
 
-form.addEventListener('reset', formToggle);
+form.addEventListener('reset', function(){
+	popup.classList.remove('popup_opened');
+});
 form.addEventListener('submit', formSubmit);
 buttonEdit.addEventListener('click', formOpen);
